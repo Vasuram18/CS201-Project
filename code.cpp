9c590953db93451db96d0c93ab6f8f3f20dc62de
mahindra4 <139013310+mahindra4@users.noreply.github.com>
2023-11-06 16:57:34 +0530

    Add files via upload

---
+++ code.cpp
@@ -0,0 +1,156 @@
+#include <bits/stdc++.h>
+#include <chrono>
+using namespace std;
+pair<int,int> find(pair<int,int> x,vector<vector<pair<int,int>>> &v){
+    //union find using path compression
+    pair<int,int> p = x;
+    while(v[p.first][p.second]!=p){
+        p = v[p.first][p.second];
+    }
+    while(x!=p){
+        pair<int,int> parent = v[x.first][x.second];
+        v[x.first][x.second] = p;
+        x = parent;
+    }
+    return x;
+}
+void Union(pair<int,int> a,pair<int,int> b,vector<vector<pair<int,int>>> &v){
+    pair<int,int> p = find(a,v);
+    v[p.first][p.second]=find(b,v);
+}
+namespace std {
+    template <>
+    struct hash<std::pair<std::pair<int, int>, std::pair<int, int>>> {
+        size_t operator()(const std::pair<std::pair<int, int>, std::pair<int, int>>& p) const {
+            size_t hash1 = std::hash<int>()(p.first.first);
+            size_t hash2 = std::hash<int>()(p.first.second);
+            size_t hash3 = std::hash<int>()(p.second.first);
+            size_t hash4 = std::hash<int>()(p.second.second);
+            return hash1 ^ (hash2 << 1) ^ (hash3 << 2) ^ (hash4 << 3);
+        }
+    };
+}
+// struct change{
+//     int n;
+//     change(int n): n(n) {}
+//     bool operator()(pair<int,int> a,pair<int,int> b){
+//         int costa = sqrt((n-1-a.first)*(n-1-a.first)+(n-1-a.second)*(n-1-a.second));
+//         int costb = sqrt((n-1-b.first)*(n-1-b.first)+(n-1-b.second)*(n-1-b.second));
+//         if(costa<costb){
+//             return b<a;
+//         }
+//         else{
+//             return a<b;
+//         }
+//     }
+// };
+struct change{
+    int n;
+    change(int n): n(n) {}
+    bool operator()(pair<int,int> a,pair<int,int> b){
+        int costa = 2*(n-1)-(a.first+a.second);
+        int costb = 2*(n-1)-(b.first+b.second);
+        if(costa<costb){
+            return b<a;
+        }
+        else{
+            return a<b;
+        }
+    }
+};
+unordered_set<pair<pair<int,int>,pair<int,int>>> a_path(unordered_set<pair<pair<int,int>,pair<int,int>>> maze,int n){
+    vector<vector<bool>> visited(n,vector<bool>(n,false));
+    vector<vector<pair<int,int>>> parent(n,vector<pair<int,int>>(n,{-1,-1}));
+    change customComparator(n);
+    priority_queue<pair<int,int>,vector<pair<int,int>>, change> st(customComparator);
+    st.push({0,0});
+    visited[0][0]=true;
+    while(!st.empty()){
+        pair<int,int> p = st.top();
+        st.pop();
+        int i = p.first,j = p.second;
+        // cout<<i<<" "<<j<<endl;
+        visited[i][j]=true;
+        if(p==make_pair(n-1,n-1)) break;
+        a_count++;
+        if(i<n-1 && maze.find({{i+1,j},{i+1,j+1}})==maze.end() && !visited[i+1][j]){
+            st.push({i+1,j});
+            parent[i+1][j]={i,j};
+        }
+        if(i>0 && maze.find({{i,j},{i,j+1}})==maze.end() && !visited[i-1][j]){
+            st.push({i-1,j});
+            parent[i-1][j]={i,j};
+        }
+        if(j<n-1 && maze.find({{i,j+1},{i+1,j+1}})==maze.end() && !visited[i][j+1]){
+            st.push({i,j+1});
+            parent[i][j+1]={i,j};
+        }
+        if(j>0 && maze.find({{i,j},{i+1,j}})==maze.end() && !visited[i][j-1]){
+            st.push({i,j-1});
+            parent[i][j-1]={i,j};
+        }
+    }
+    pair<int,int> end = {n-1,n-1};
+    unordered_set<pair<pair<int,int>,pair<int,int>>> path;
+    while(parent[end.first][end.second]!=make_pair(-1,-1)){
+        pair<int,int> a = parent[end.first][end.second];
+        if(end.first==a.first+1||end.second==a.second+1){
+            path.insert({a,end});
+        }
+        else{
+            path.insert({end,a});
+        }
+        end = a;
+    }
+    return path;
+}
+int main(){
+    int n;
+    cout<<"Enter the dimension of the maze(nxn): ";
+    cin>>n;
+    vector<vector<int>> v(n,vector<int>(n));
+    vector<pair<pair<int,int>,pair<int,int>>> edges;
+    set<pair<int,int>> visited;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(i<n-1 && visited.find({i,j})==visited.end()){
+                edges.push_back({{i,j},{i+1,j}});
+            }
+            if(j<n-1 && visited.find({i,j})==visited.end()){
+                edges.push_back({{i,j},{i,j+1}});
+            }
+            visited.insert({i,j});
+        }
+    }
+    auto start = chrono::high_resolution_clock::now();
+    random_device rd;
+    mt19937 g(rd());
+    shuffle(edges.begin(),edges.end(),g);
+
+    vector<vector<pair<int,int>>> parent(n,vector<pair<int,int>>(n));
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+           parent[i][j] = {i,j};
+        }
+    }
+    unordered_set<pair<pair<int,int>,pair<int,int>>> path;
+    for(int i=0;i<edges.size();i++){
+        pair<int,int> a=edges[i].first,b=edges[i].second;
+        if(find(a,parent)!=find(b,parent)){
+            Union(a,b,parent);
+            path.insert({a,b});
+        }
+    }
+    unordered_set<pair<pair<int,int>,pair<int,int>>> maze;
+    for(int i=0;i<=n;i++){
+        for(int j=0;j<=n;j++){
+            if(i<n && (path.find({{i,j-1},{i,j}})==path.end()|| j==0 || j==n)){
+                maze.insert({{i,j},{i+1,j}});
+            }
+            if(j<n && (path.find({{i-1,j},{i,j}})==path.end()|| i==0 || i==n)){
+                maze.insert({{i,j},{i,j+1}});
+            }
+        }
+    }
+    return 0;
+}
